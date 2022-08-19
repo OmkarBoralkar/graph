96ad04a94072cfa4d2463ebf8dbd5c2d9a03a4d4
ob <107201866+OmkarBoralkar@users.noreply.github.com>
2022-08-19 16:08:10 +0530

    Update g.py

--- g.py
+++ g.py
@@ -11,7 +11,7 @@ import datetime
 from matplotlib.pyplot import Circle, show
 import math
 clock=Tk()
-clock.title('                                                        Timer                                                             ')
+clock.title('                                                       Timer                                                             ')
 clock.geometry('500x100')
 clock.config(bg='skyblue')
 def time12():
@@ -21,7 +21,7 @@ def time12():
 hr=Label(clock,font=('Arial',18),bg="green",fg="yellow")
 hr.pack()
 time12()
-n=int(input("Enter the number of days you want"))
+n=int(input("Enter the number of times you want"))
 print("please enter in 24 hour format")
 for o in range(n) :
     o=int(input("Enter hour"))
@@ -35,7 +35,7 @@ for o in range(n) :
             print(" 2 for point form")
             print(" 3 for circle")
             print(" 4 for Triangle")
-            print("5 . for sorry")
+            print(" 5 for Parabola ")
             print("6. for substraction")
             print("7. for addition")
             print("8. for multiplication")
@@ -53,6 +53,7 @@ for o in range(n) :
                     plt.ylabel("plot the values of y")
                     plt.title("Slope  form")
                     plt.show()
+                    break
                 case 2:
                     x1=float(input("Enter the 1st point"))
                     x2=float(input("Enter the 2nd point"))
@@ -68,6 +69,7 @@ for o in range(n) :
                     plt.ylabel(" y axis")
                     plt.title("Two point form")
                     plt.show()
+                    break
                 case 3:
                     print("For only radius put x and y as 0")
                     g=int(input("ENTER THE CENTER of x axis "))
@@ -85,6 +87,7 @@ for o in range(n) :
                         plt.plot(x,y)
                         plt.title("Circle with centre and radius")
                         plt.show()
+                        break
                 case 4:
                     def triangle():
                         n1=int(input("Enter the 1s side of triangle"))
@@ -118,10 +121,78 @@ for o in range(n) :
                             plt.title(" scalene triangle")
                             plt.show()
                     triangle()
+                    break
                 case 5:
-                    n=int(input("Enter the number"))
-                    for i in range (n):
-                        print("Sorry")
+                    print("1. for x axis")
+                    print("2. for y axis")
+                    r=int(input("Enter the operation"))
+                    if r>2 :
+                        print("Invalid")
+                    match r:
+                        case 1:
+                            a=float(input("Enter the value of a"))
+                            c=float(input("Enter the value of x intercept"))
+                            p=float(input("Enter the value of y intercept"))
+                            y=np.linspace(-2,10,20)
+                            if c>0:
+                                (x)=((y-p)**2)/(4*a)+c
+                                plt.plot(x,y)
+                                plt.title("Parabola along +ve X axis")
+                                plt.show()
+                            if c<0:
+                                (x)=-((y-p)**2)/(4*a)+c
+                                plt.plot(x,y)
+                                plt.title("Parabola along +ve X axis")
+                                plt.show()
+                            if c==0:
+                                print("1. +ve along x axis")
+                                print("2. -ve along x axis")
+                                j=int(input("Enter the operation"))
+                                if j>2 :
+                                    print("Invalid")
+                                match j:
+                                    case 1:
+                                        (x)=((y-p)**2)/(4*a)+c
+                                        plt.plot(x,y)
+                                        plt.title("Parabola along +ve X axis")
+                                        plt.show()
+                                    case 2:
+                                        (x)=-((y-p)**2)/(4*a)+c
+                                        plt.plot(x,y)
+                                        plt.title("Parabola along +ve X axis")
+                                        plt.show()
+                        case 2:
+                            a=float(input("Enter the value of a"))
+                            c=float(input("Enter the value of x intercept"))
+                            p=float(input("Enter the value of y intercept"))
+                            x=np.linspace(-2,0,20)
+                            if p>0:
+                                (y)=(((x-c)**2)/(4*a))+p
+                                plt.plot(x,y)
+                                plt.title("Parabola along +ve Y axis")
+                                plt.show()
+                            if p<0:
+                                (y)=-((x-c)**2)/(4*a)+p
+                                plt.plot(x,y)
+                                plt.title("Parabola along +ve Y axis")
+                                plt.show()
+                            if p==0:
+                                print("1. +ve along y axis")
+                                print("2. -ve along y axis")
+                                j=int(input("Enter the operation"))
+                                if j>2 :
+                                    print("Invalid")
+                                match j:
+                                    case 1:
+                                        (y)=((x-c)**2)/(4*a)+p
+                                        plt.plot(x,y)
+                                        plt.title("Parabola along +ve Y axis")
+                                        plt.show()
+                                    case 2:
+                                        (y)=-((x-c)**2)/(4*a)+p
+                                        plt.plot(x,y)
+                                        plt.title("Parabola along +ve Y axis")
+                                        plt.show()
                 case 6:
                     q=int(input("Enter the number of inputs you want"))
                     a=[]
@@ -146,6 +217,7 @@ for o in range(n) :
                     print('\n',v)
                     c=r*f
                     print(c)
+                    break
                 case 7:
                     q=int(input("Enter the number of inputs you want"))
                     a=[]
@@ -170,6 +242,7 @@ for o in range(n) :
                     print('\n',v)
                     c=r*f
                     print(c)
+                    break
                 case 8:
                     q=int(input("Enter the number of inputs you want"))
                     a=[]
@@ -194,6 +267,6 @@ for o in range(n) :
                     print('\n',v)
                     c=r*f
                     print(c)
-
+                    break
             break  
 mainloop()
